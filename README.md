79d9a03e1203b0f485d85b63698a812a23f663c1
root <root@localhost.localdomain>
2021-03-31 17:11:08 -0400

    weather api

---
+++ README.md
@@ -0,0 +1,15 @@
+# weather_api
+
+Please install python3, pip3 and install pyowm 
+
+PyOWM is a client Python wrapper library for OpenWeatherMap (OWM) web APIs. It allows quick and easy consumption of OWM data from Python applications via a simple object model and in a human-friendly fashion.
+
+python3 weather.py
+
+Example output:
+
+*********Philadlephia, Pennsylvania, United States*********  
+Temperature: 16 °C | 61 °F  
+Humidity: 94 %  
+
+![](image_result.gif)
---
+++ weather.py
@@ -0,0 +1,13 @@
+import pyowm
+
+owm = pyowm.OWM('SECRET_API_KEY')
+observation = owm.weather_at_place('Philadelphia, Pennsylvania, United States')
+w = observation.get_weather()
+tempC = w.get_temperature('celsius')
+tempF = w.get_temperature('fahrenheit')
+status = w.get_detailed_status()
+humidity = w.get_humidity()
+
+print('*********Philadlephia, Pennsylvania, United States*********')
+print('Temperature: ' + str(int(tempC['temp'])) + ' \N{DEGREE SIGN}C | ' + str(int(tempF['temp'])) + ' \N{DEGREE SIGN}F')
+print('Humidity: ' + str(humidity) + ' %')
